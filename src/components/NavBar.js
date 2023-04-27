2c1e2929c69d83d94aeaef0fb5fdaf2a2bc72387
geoffrey kinuthia <geoffreykinuthia10@gmail.com>
2023-04-27 10:13:34 +0300

    final commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,25 @@
 import React from "react";
-
+// defining a functional component "Navbar"
 function NavBar() {
+  // defining an array of links 
+  // the array to be included the navigation bar
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  // rendering the navigation bar a a list of tags
+  // with each link as a separate item
+  return (
+    <nav>
+      {/* using map to iterate over each element  */}
+      {/* using index of each array as its unique key, and set the href attribute to the link text */}
+      {links.map((link, index) => (
+        <a key={index} href={`#${link}`}>
+          {link}
+        </a>
+      ))}
+    </nav>
+  );
 }
 
+
+// exporting the Navbar component as the default export of the module
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,20 @@
 import React from "react";
+// importing the ProjectItem component.
 import ProjectItem from "./ProjectItem";
-
+// defining the components function and passing in the projects prop 
 function ProjectList({ projects }) {
+  // tring to log in the projectto the console to see its function 
   console.log(projects);
   return (
+    // rendring the main container for the project section with an ID of projects
     <div id="projects">
+      {/* rendering heder for section */}
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {/* mapping over the projects prop to render the projectItem component "iteratably" for each */}
+        {projects.map((project) => (
+          <ProjectItem key={project.id} name={project.name} about={project.about} technologies={project.technologies}/>
+        ))}</div>
     </div>
   );
 }
